d14bbe19b48295333fd4f228e3850914589fbd00
Leroy G <liguan698@gmail.com>
2023-03-13 23:21:30 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
 // Write your code here
+let num1 = 2, num2 = 31;
+let multiply = 2 * 31;
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 19, num4 = 5;
+let mod = 19 % 5;
+let max = Math.max(2,4,5,7,20)
